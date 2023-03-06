93156ae46d66ca67ec6d5ace6cd0469cbd250720
Benjamin Pilbrow <bpilbrow@yahoo.co.nz>
2023-03-07 11:23:35 +1300

    DO NOT OPEN
    Very broken, will fix soon. :)

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,59 +1,104 @@
-var x=300;
-var y=300;
-var a=100;
-var b=100;
+let streams = [];
+let symbolSize = 20;
+
+
+function setup() {
+	  //createCanvas(windowWidth, windowHeight);
+	  background(0);
+	
+	  let x = 0;
+	  for (let i = 0; i <= width / symbolSize; i++) {
+		let stream = new Stream();
+		stream.generateSymbols(x, random(-1000, 0));
+		streams.push(stream);
+		x += symbolSize;
+	  }
+	
+	  textSize(symbolSize);
+	}
 
 function draw_one_frame() {
-  //background(255);
-  x+=2;
-  y+=2;
-	a-=2;
-	b-=2;
 
-	strokeWeight(1);
-  translate(width/2, height/2);
-
-  for(var i=0;i<15;i++){
-	  for(var k=0;k<20;k++){
-
-		stroke(255,255,255);
-		rotate(PI / 12.0);
-		fill(255,255-i*10,255-k*10);
-		line(a%100,b%100,x%300,y%300); // old white lines in center 
-
-		ellipse((x+i*20)%width,(y+k*20)%height,i+4,i+4);
-
-// 		drawtriangle((a-i*20)%width,(b-k*20)%height,k/8);
-
-		rect(x%width, y%height, k+10, k+10);
-
-		fill(0,i*10,255-k*10);
-		ellipse((x-i*20)%width,(y-k*20)%height,i+3,i+3);
-		
-// 		rotate(PI / 24.0);
-// 		fill(255-(i+k)*5,(i+k)*7,i*20);
-// 		drawtriangle((a+i*20)%width,(b+k*20)%height,k/8);
-// 		rect(a%width, b%height, k+5, k+5);
-// 		drawflower(k,x);
+	
+	
+	
+	function draw() {
+	  background(0, 150);
+	  streams.forEach(function(stream) {
+		stream.render();
+	  });
+	}
+	
+	class Symbol {
+	  constructor(x, y, speed, first, opacity) {
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.first = first;
+		this.opacity = opacity;
+		this.switchInterval = round(random(2, 25));
 	  }
-
-  }
-
-}
-
-function drawtriangle(x,y,r){
-	triangle(x, y, x+7*r, y-13.75*r, x+14*r, y);
-}
-
-function drawflower(i,k){
-		if(i%2==1){
-			fill(255,(k*0.4)%255,30);
-			stroke(k%255,255,0);
-			arc(0,0,150+i+150*sin(k*PI/24),150,0,PI / 40);
+	
+	  setToRandomSymbol() {
+		let charType = round(random(0, 5));
+		if (frameCount % this.switchInterval == 0) {
+		  if (charType > 1) {
+			this.value = String.fromCharCode(
+			  0x30A0 + round(random(0, 96))
+			);
+		  } else {
+			this.value = round(random(0, 9));
+		  }
 		}
-		else{
-			fill(k%255,0,255);
-			stroke(0,(k*0.4)%255,255);
-			arc(0,0,(100+100*cos(k*PI/24))%255,50,0,PI / 20);
+	  }
+	
+	  rain() {
+		if (this.y >= height) {
+		  this.y = 0;
+		} else {
+		  this.y += this.speed;
 		}
+	  }
+	}
+	
+	class Stream {
+	  constructor() {
+		this.symbols = [];
+		this.totalSymbols = round(random(5, 30));
+		this.speed = random(5, 15);
+	  }
+	
+	  generateSymbols(x, y) {
+		let opacity = 255;
+		let first = round(random(0, 4)) == 1;
+		for (let i = 0; i <= this.totalSymbols; i++) {
+		  let symbol = new Symbol(
+			x,
+			y,
+			this.speed,
+			first,
+			opacity
+		  );
+		  symbol.setToRandomSymbol();
+		  this.symbols.push(symbol);
+		  y -= symbolSize;
+		  opacity -= 255 / this.totalSymbols / fadeInterval;
+		  first = false;
+		}
+	  }
+	
+	  render() {
+		this.symbols.forEach(function(symbol) {
+		  if (symbol.first) {
+			fill(120, 255, 120, symbol.opacity);
+		  } else {
+			fill(0, 255, 70, symbol.opacity);
+		  }
+		  text(symbol.value, symbol.x, symbol.y);
+		  symbol.rain();
+		  symbol.setToRandomSymbol();
+		});
+	  }
+	}
+
 }
