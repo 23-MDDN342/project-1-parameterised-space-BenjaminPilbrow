53a0d65c8fe86fa99f2dcf9ffe44104bd3c89afe
Benjamin Pilbrow <bpilbrow@yahoo.co.nz>
2023-03-13 21:37:55 +1300

    transferred my code over from test project into the framework
    it broken! will need to look into issues tomorrow in class

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,104 +1,87 @@
-let streams = [];
-let symbolSize = 20;
-
+let symbolSize = 60; // set symbol size
+let streams = []; // create array for the streams
 
 function setup() {
-	  //createCanvas(windowWidth, windowHeight);
-	  background(0);
-	
-	  let x = 0;
-	  for (let i = 0; i <= width / symbolSize; i++) {
-		let stream = new Stream();
-		stream.generateSymbols(x, random(-1000, 0));
-		streams.push(stream);
-		x += symbolSize;
-	  }
-	
-	  textSize(symbolSize);
-	}
 
-function draw_one_frame() {
+  createCanvas(
+    window.innerWidth,
+    window.innerHeight
+  );
 
-	
-	
-	
-	function draw() {
-	  background(0, 150);
-	  streams.forEach(function(stream) {
-		stream.render();
-	  });
-	}
-	
-	class Symbol {
-	  constructor(x, y, speed, first, opacity) {
-		this.x = x;
-		this.y = y;
-		this.speed = speed;
-		this.first = first;
-		this.opacity = opacity;
-		this.switchInterval = round(random(2, 25));
-	  }
-	
-	  setToRandomSymbol() {
-		let charType = round(random(0, 5));
-		if (frameCount % this.switchInterval == 0) {
-		  if (charType > 1) {
-			this.value = String.fromCharCode(
-			  0x30A0 + round(random(0, 96))
-			);
-		  } else {
-			this.value = round(random(0, 9));
-		  }
-		}
-	  }
-	
-	  rain() {
-		if (this.y >= height) {
-		  this.y = 0;
-		} else {
-		  this.y += this.speed;
-		}
-	  }
-	}
-	
-	class Stream {
-	  constructor() {
-		this.symbols = [];
-		this.totalSymbols = round(random(5, 30));
-		this.speed = random(5, 15);
-	  }
-	
-	  generateSymbols(x, y) {
-		let opacity = 255;
-		let first = round(random(0, 4)) == 1;
-		for (let i = 0; i <= this.totalSymbols; i++) {
-		  let symbol = new Symbol(
-			x,
-			y,
-			this.speed,
-			first,
-			opacity
-		  );
-		  symbol.setToRandomSymbol();
-		  this.symbols.push(symbol);
-		  y -= symbolSize;
-		  opacity -= 255 / this.totalSymbols / fadeInterval;
-		  first = false;
-		}
-	  }
-	
-	  render() {
-		this.symbols.forEach(function(symbol) {
-		  if (symbol.first) {
-			fill(120, 255, 120, symbol.opacity);
-		  } else {
-			fill(0, 255, 70, symbol.opacity);
-		  }
-		  text(symbol.value, symbol.x, symbol.y);
-		  symbol.rain();
-		  symbol.setToRandomSymbol();
-		});
-	  }
-	}
+  background (0) // set background colour
+  let x = 0;
+  for (let i = 0; i <= width / symbolSize; i++) {
+    let stream = new Stream();
+    stream.generateSymbols(x, random(-1000, 0)); // generate the symbols at a random y value
+    streams.push(stream);
+    x += symbolSize; 
+  }
+  textSize(symbolSize);
+}
+
+function draw() {
+  background(0, 100);
+  streams.forEach(function(stream) {
+    stream.render();
+  });
+}
+
+function Symbol(x, y, speed, first) {
+  this.x = x; // x pos
+  this.y = y; // y pos
+  this.value; // stores which symbol is displayed
+
+  this.speed = speed; // how fast the symbol moves
+  this.first = first; // changes the first symbol in each stream
+
+  this.swtichInterval = round(random(2, 20));
+  
+  this.setToRandomSymbol = function() {
+    if (frameCount % this.swtichInterval == 0) { // whenever switchInterval divides evenly into frameCount, then run code
+      this.value = String.fromCharCode(
+        0x30A0 + round(random(0, 96)) //select random unicode charater from 0 to 96
+      );
+    }
+  }
+  
+  this.rain = function() {
+    if (this.y >= height){ // checks to see if the symbol has reached the bottom, in which case it will reset back to the top
+      this.y = 0;
+    } else {
+      this.y += this.speed;
+    }
+  }
+}
+
+function Stream(){
+  this.symbols = [];
+  this.totalSymbols = round(random(5, 35)); // select total number of symbols for each row
+  this.speed = random(5, 25); // select random speed for symbols
+
+  this.generateSymbols = function(x, y) {
+    let first = round(random(0, 4)) == 1; // check if number is == to 1, if so set to true otherwise set to false
+    for (let i =0; i <= this.totalSymbols; i++) {
+      symbol = new Symbol(x, y, this.speed, first);
+      symbol.setToRandomSymbol();
+      this.symbols.push(symbol);
+      y -= symbolSize;
+      first = false; // change variable to false after first loop
+    }
+  }
+
+  this.render = function() {
+    
+    this.symbols.forEach(function(symbol) {
+      if (symbol.first) {
+        fill(180, 255, 180);
+      } else {
+        fill(0, 255, 70); // symbol colour, green
+      }
+      text(symbol.value, symbol.x, symbol.y); // display the symbol as text
+      symbol.rain(); // call the rain function
+      symbol.setToRandomSymbol();
+    });
+
+  }
 
 }
