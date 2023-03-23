65851de9876e0152705e86bb1271925b495dd764
Benjamin Pilbrow <bpilbrow@yahoo.co.nz>
2023-03-23 17:50:13 +1300

    Final Commit

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10300689&assignment_repo_type=AssignmentRepo)
 ### 2023 MDDN342 Project 1: Parameterised Space
 
-Added a background stream into the code, with a lower brightness and smaller font size. Also made the font size / by the width, so that it stays the same no matter what size the canvas is. Next I'll mess around with making the background colour chnage slightly with the hue of the text, so we'll see how that goes!
+MDDN342 - Project 1 - MATRIX RAINING TEXT ZOOM BACKGROUND
+
+INTRODUCTION
+For Project 1 of Creative Coding 3, I decided to recreate the "Matrix Raining Text" animation effect using a p5.js sketch, with the intent to produce an animated Zoom background that seamlessly loops every 1 second. I have parameterised it to work at any scale, with options to easily change the colours, speed, and amount of characters per stream, along with a plethora of other additions. 
+
+
+GOALS
+My goals for this project were simple: create a parameterised looping 1 second animation in the style of The Matrix that I can use as a Zoom background, with a options to customise the design (as outlined above).
+
+
+PROCESS
+Starting out, I knew what effect I wanted to achieve, however I didn't know exactly where to start. This is where some internet searching helped. Orginally, I asked ChatGPT for some guidance on how I would go about creating an animation like this, and it provided me with some code that got me on (somewhat) of the right track. The code was completely broken however, so I stashed that work and started again. Next I stumbled across a fantastic video by The Coding Train (https://youtu.be/S1TQCi9axzg), which helped me to get a great head start on how to lay out my code. 
+
+Of course, the framework we were working within for the project ment I couldn't simply copy all my work over from the video, and I spent a fair amount of time trying to get the animation running within the framework. Eventually, with some help from Phoebe, I got it working and was able to start refining it down.
+
+The key areas I focused on were making it actually loop, adjust how the text size was set (as it wasn't scaling correctly), adding options for changing colours, and more. After a number of iterations (see the Progress Document), I ended up with a design I am rather pleased with. 
+
+
+FINAL THOUGHTS
+Overall I enjoyed the simple nature of my design in this project. While at times the design was quite limiting, it was a fun challange to get it all working within the framework and I find the final output to be very satisfying to look at for extended periods of time. 
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,55 +1,63 @@
 let charSize = 0 //20; // set char size
 let streams = []; // create array for the streams
-let firstRun  = true;
-let hue = 0;
-let hueChange = true;
+let firstRun  = true; // checks to see if first run
+let hue = 0; // hue value for text colour
+let hueChange = true; // set hueChange to true
 
-
+//-------DRAW_ONE_FRAME-------//
 function draw_one_frame() {
-if(firstRun){
-  charSize = width / 100
-  background (0) // set background colour
-  let x = 0;
-  for (let i = 0; i <= width / charSize; i++) {
-    let stream = new Stream();
-    stream.generateChars(x, random(-1000, 0)); // generate the symbols at a random y value
-    streams.push(stream);
-    x += charSize; 
+  if(firstRun){
+    charSize = width / 100 // set text size to scale with screen size
+    textSize(charSize); // apply text size
+
+    let x = 0;
+
+    // loop for generating streams == to the width of the screen
+    for (let i = 0; i <= width / charSize; i++) {
+      let stream = new Stream();
+      stream.generateChars(x, random(-1000, 0)); // generate the symbols at a random y value
+      streams.push(stream);
+      x += charSize; 
+    }
+
+    firstRun = false; // set firstRun to false, so above only runs the first time
   }
-  textSize(charSize);
-firstRun = false;
-}
-  background(20);
+
+  background(20); // set background colour
+
+  // render each stream
   streams.forEach(function(stream) {
-    stream.render();
-    stream.render2();
+    stream.render(); // main stream
+    stream.render2(); // background stream
   });
 
+  // hue text colour logic
   if (hue == 0){
-    hueChange = true
+    hueChange = true // when hue == 0, set to true
   } else if (hue == 100) {
-    hueChange = false
+    hueChange = false // when hue == 100, set to false
   }
 
   if (hueChange == true) {
-    hue = hue + 5;
+    hue = hue + 5; // if hueChange == true, keep adding 5 to the value of hue
   }
   else if (hueChange == false) {
-    hue = hue - 5;
+    hue = hue - 5; // if hueChange == false, keep subtracting 5 from the value of hue
   }
-
 }
 
+//-------Char-------//
 function Char(x, y, speed, first) {
-  this.x = x; // x pos
-  this.y = y; // y pos
-  this.value; // stores which symbol is displayed
+  this.x = x; // character x pos
+  this.y = y; // character y pos
+  this.value; // stores which character is displayed
 
-  this.speed = speed; // how fast the symbol moves
-  this.first = first; // changes the first symbol in each stream
+  this.speed = speed; // how fast the character moves
+  this.first = first; // handles the first character in each stream
 
-  this.swtichInterval = round(random(2, 20));
+  this.swtichInterval = round(random(2, 20)); // how fast the character changes
   
+  // handles changing the character
   this.setToRandomChar = function() {
     if (frameCount % this.swtichInterval == 0) { // whenever switchInterval divides evenly into frameCount, then run code
       this.value = String.fromCharCode(
@@ -58,6 +66,7 @@ function Char(x, y, speed, first) {
     }
   }
   
+  // handles the raining animation
   this.rain = function() {
     if (this.y >= height){ // checks to see if the symbol has reached the bottom, in which case it will reset back to the top
       this.y = 0;
@@ -67,54 +76,61 @@ function Char(x, y, speed, first) {
   }
 }
 
+//-------Stream-------//
 function Stream(){
-  this.chars = [];
-  this.totalChars = 8//round(random(5, 35)); // select total number of symbols for each row
-  this.speed = 25//random(30, 50); // select random speed for symbols
+  this.chars = []; // array for characters
+  this.totalChars = 8 // set total number of characters for each row
+  this.speed = 25 // set falling speed for characters
 
+  // handles generating characters
   this.generateChars = function(x, y) {
     let first = 1 == 1; // check if number is == to 1, if so set to true otherwise set to false
     for (let i =0; i <= this.totalChars; i++) {
       char = new Char(x, y, this.speed, first);
-      char.setToRandomChar();
+      char.setToRandomChar(); // set to new random character
       this.chars.push(char);
-      y -= charSize;
+      y -= charSize; // move each character above previous 
       first = false; // change variable to false after first loop
     }
   }
 
+  // render information for main stream
   this.render = function() {
-    
     this.chars.forEach(function(char) {
-      colorMode(HSB)
-      if (char.first) {
-        fill(hue, 255, 255);
-      } else {
-        fill(hue, 255, 50); // symbol colour, green
-      }
-      textSize(charSize)
-      text(char.value, char.x, char.y); // display the symbol as text
-      char.rain(); // call the rain function
-      char.setToRandomChar();
-    });
+      colorMode(HSB) // set colour mode to HSB
 
+      if (char.first) {
+        fill(hue, 255, 255); // set first character to different colour (brighter)
+      } else {
+        fill(hue, 255, 50); // set all other character colours
+      }
+
+      textSize(charSize) // set text size
+      text(char.value, char.x, char.y); // display the symbol as text
+
+      char.rain(); // call the rain function
+      char.setToRandomChar(); // call random character function
+    });
   }
 
+  // render information for background stream
   this.render2 = function() {
     
     this.chars.forEach(function(char) {
-      colorMode(HSB)
+      colorMode(HSB) // set colour mode to HSB
+
       if (char.first) {
-        fill(hue+20, 255, 100);
+        fill(hue+20, 255, 100); // set first character to different colour (brighter)
       } else {
-        fill(hue+20, 255, 25); // symbol colour, green
+        fill(hue+20, 255, 25); // set all other character colours
       }
-      textSize(charSize - 5)
+
+      textSize(charSize - 5) // set text size, but slightly smaller
       text(char.value, char.x + 100, char.y + 100); // display the symbol as text
+
       char.rain(); // call the rain function
-      char.setToRandomChar();
+      char.setToRandomChar(); // call random character function
     });
-
   }
-
+  
 }
